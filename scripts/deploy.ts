cb0690ea007454f98eb32c327a132d685ab46839
Cofeesy <2898939720@qq.com>
2023-01-08 19:59:54 +0800

    scripts\deploy.ts contracts\myExchange.sol

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,10 +3,18 @@ import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const ft = await FT.deploy("WangZheng-ERC20", "WZ-ERC20");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  //await ft.deployed();
+  //console.log(`FT deployed to ${ft.address}`);
+
+  //5.部署myExchange合约:
+  const Exchange = await ethers.getContractFactory("Exchange");
+  const myExchange = await Exchange.deploy(ft.address);
+
+  await myExchange.deployed();
+
+  console.log(`myExchange deployed to ${myExchange.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
