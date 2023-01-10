87e264cceaea84eb4306de7a7b5fa1c4e3996651
GanZanXu <848246211@qq.com>
2023-01-10 23:23:27 +0800

    uniswap+staker

---
+++ contracts/core/Factory.sol
@@ -0,0 +1,41 @@
+pragma solidity =0.5.16;
+
+import './interfaces/IFactory.sol';
+import './Pair.sol';
+
+//由于仅仅是实验，不考虑协议费的收取
+contract Factory is IGzxFactory {
+
+    bytes public PairBytecode;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor() public {
+        PairBytecode = type(Pair).creationCode;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+}
