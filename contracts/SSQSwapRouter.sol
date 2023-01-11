0b1a2e9de43deb37c3fa4a40c0969a5517b90d9c
宋斯琪 <1239119811@qq.com>
2023-01-11 22:44:20 +0800

    router返回pair的地址

--- contracts/SSQSwapRouter.sol
+++ contracts/SSQSwapRouter.sol
@@ -12,12 +12,14 @@ contract SSQSwapRouter {
         factory = SSQSwapFactory(_factory);
     }
 
-    function createPair(address token0, address token1, uint256 token0Amount, uint256 token1Amount) public {
+    function createPair(address token0, address token1, uint256 token0Amount, uint256 token1Amount) public returns (address) {
         SSQSwapPair pair = SSQSwapPair(factory.createPair(token0, token1));
 
         address account = msg.sender;
         ERC20(token0).transferFrom(account, address(pair), token0Amount);
         ERC20(token1).transferFrom(account, address(pair), token1Amount);
         factory.initializePair(address(pair), account);
+
+        return address(pair);
     }
 }
