8767bba48570dc337e2eb013b87e81b2db95cbd8
Wchang122 <106457317+Wchang122@users.noreply.github.com>
2023-01-15 22:38:42 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,14 +7,16 @@ async function main() {
 
 //   await ft.deployed();
 //   console.log(`FT deployed to ${ft.address}`);
-// }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 
-  const Exchange = await ethers.getContractFactory("Exchange");
+  const Exchange = await ethers.getContractFactory("TokenExchange");
   const TokenExchange = await Exchange.deploy(ft.address);
+  
+  await ft.deployed();
+  console.log(`FT deployed to ${ft.address}`);
 
   await TokenExchange.deployed();
-
   console.log(`TokenExchange deployed to ${TokenExchange.address}`);
+  }
