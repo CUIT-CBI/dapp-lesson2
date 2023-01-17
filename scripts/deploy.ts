7e0390c92a86084af4ec9383f2fa7e55709a0220
18079827430 <2857589525@qq.com>
2023-01-17 08:46:42 +0800

    modify

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,6 @@ import { ethers } from "hardhat";
 
 async function main() {
 
-<<<<<<< HEAD
   const Token = await ethers.getContractFactory("WuChang");
   const token = await Token.deploy();
 
@@ -16,20 +15,4 @@ async function main() {
   await exchange.deployed();
   console.log(`exchange deployed to ${exchange.address}`);
   }
-=======
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
 
-  const Token = await ethers.getContractFactory("token");
-  const DeployToken = await Token.deploy();
-
-  await DeployToken.deployed();
-  console.log(`token deployed to ${DeployToken.address}`);
-
-  const Exchange = await ethers.getContractFactory("TokenExchange");
-  const TokenExchange = await Exchange.deploy(DeployToken.address);
-
-  await TokenExchange.deployed();
-  console.log(`TokenExchange deployed to ${TokenExchange.address}`);
-  }
->>>>>>> a8a9cc311c2690034eb386fad7c00f0b5bc39ed7
