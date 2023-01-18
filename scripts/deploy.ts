645bc9a65424c768d44090f3b5b03698686e5862
sosott <117749370+sosott@users.noreply.github.com>
2023-01-18 21:02:37 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,12 +3,18 @@ import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const token0 = await FT.deploy("token0", "ZSY");
+  await token0.deployed();
+  const token1 = await FT.deploy("token2", "ZSY");
+  await token1.deployed();
+  console.log(`token0 deployed to ${token0.address}`);
+  console.log(`token1 deployed to ${token1.address}`);
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  const Pair = await ethers.getContractFactory("pair");
+  const pair = await Pair.deploy(token0.address, token1.address);
+  await pair.deployed();
+  console.log(`Pair deployed to ${pair.address}`);
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
