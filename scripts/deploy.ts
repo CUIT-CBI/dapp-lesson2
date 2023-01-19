30ce1693814aa77d35dad71cea836d6355280df9
mygm-123455 <2492311024@qq.com~>
2023-01-19 18:44:01 +0800

    2020131157

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,16 +2,25 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  const Token1 = await ethers.getContractFactory("FT");
+  const token1 = await Token1.deploy("Token1", "token1");
+  await token1.deployed();
+  console.log(`Token1: ${token1.address}`);
+
+  const Token2 = await ethers.getContractFactory("FT");
+  const token2 = await Token2.deploy("Token2", "token2");
+  await token2.deployed();
+  console.log(`Token2: ${token2.address}`);
+
+  const Factory = await ethers.getContractFactory("Exchange");
+  const factory = await Factory.deploy(token1.address,token2.address);
+  await factory.deployed();
+  console.log(`Uniswap: ${factory.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
