e844412774f0eddcf9ad217b073879ea446373a6
TickledPinkZyh <109411375+TickledPinkZyh@users.noreply.github.com>
2023-01-19 11:37:34 +0800

    2020131133
    2020131133

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,24 @@
 import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
-
+let deployer
 async function main() {
   const FT = await ethers.getContractFactory("FT");
   const ft = await FT.deploy("CBI", "CUIT");
+  const token1 = await FT.deploy("CBI", "CUIT");
+  const token2 = await FT.deploy("ZYH", "ZYH");
+  const Factory = await ethers.getContractFactory("Factory");
+  const factory = await Factory.deploy(ft.address);
 
   await ft.deployed();
   console.log(`FT deployed to ${ft.address}`);
+  await token1.setPair(deployer.address)
+  await token2.setPair(deployer.address)
+  await token1.mint(deployer.address,10000000)
+  await token2.mint(deployer.address,10000000)
+  await token1.approve(factory.address,10000000)
+  await token2.approve(factory.address,10000000)
+  await factory.deployed();
+  console.log(`Factory deployed to ${factory.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
