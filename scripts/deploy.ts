6d937805005561535eb0bf1e6de7b7429dcae8b5
csmcool <1298911288@qq.com>
2023-01-19 18:03:23 +0800

    add

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,32 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
+  const UniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory");
+  const uniswapV2Factory = await UniswapV2Factory.deploy();
+
+  await uniswapV2Factory.deployed();
+
+  const WETH = await ethers.getContractFactory("WETH9");
+  const wETH = await WETH.deploy();
+
+  await wETH.deployed();
+
+  const Uniswap = await ethers.getContractFactory("uniswap");
+  const uniswap = await  Uniswap.deploy(uniswapV2Factory.address,wETH.address);
+
+  await uniswap.deployed();
+
   const FT = await ethers.getContractFactory("FT");
   const ft = await FT.deploy("CBI", "CUIT");
 
   await ft.deployed();
+
+
   console.log(`FT deployed to ${ft.address}`);
+  console.log(`UniswapV2Factory deployed to ${uniswapV2Factory.address}`);
+  console.log(`WETH deployed to ${wETH.address}`);
+  console.log(`Uniswap deployed to ${uniswap.address}`);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
