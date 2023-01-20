642286ac95d4449d5c4dec43d27efae439b84431
YangRuoyu2001 <119563122+YangRuoyu2001@users.noreply.github.com>
2023-01-20 10:53:06 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,10 +3,24 @@ import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const token1 = await FT.deploy("CBI", "CUIT");
+  const token2 = await FT.deploy("YangRuoyu", "YRY");
+  
+  await token1.deployed();
+  await token2.deployed();
+  
+  console.log(`token1 deployed to ${token1.address}`);
+  console.log(`token2 deployed to ${token2.address}`);
+  
+  //部署pool
+  const pool = await ethers.getContractFactory("pool");
+  //得到demo
+  const demo = await pool.deploy(token1.address, token2.address);
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  await demo.deployed();
+  console.log(`demo deployed to ${demo.address}`);
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
