d871e0cb738587e0e3ea8afa8fd8520b77cc0cc3
skcyze <116003311+skcyze@users.noreply.github.com>
2023-01-20 17:52:46 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,27 @@
 import '@nomiclabs/hardhat-ethers';
+import { Contract } from 'ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  let TokenA = await ethers.getContractFactory("FT");
+  let ContrcatTokenA = await TokenA.deploy("tokenA","WWZ");
+  let TokenB = await ethers.getContractFactory("FT");
+  let ContrcatTokenB = await TokenB.deploy("tokenB","WWZ");
+  let wwzExchange = await ethers.getContractFactory("FT");
+  let ContrcatExchange = await wwzExchange.deploy(ContrcatTokenA.address, ContrcatTokenB.address);
+
+  let addrA = ContrcatTokenA.address;
+  let addrB = ContrcatTokenB.address;
+  let addrExchange = ContrcatExchange.address;
+
+  console.log('addrTokenA is :', addrA);
+  console.log('addrTokenB is :', addrB);
+  console.log('addrwwzExchange is :', addrExchange);
+
 }
 
+ 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
