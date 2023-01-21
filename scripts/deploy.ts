32c4cc24a105dea8b73f9aca0b6d15ce9f728188
Kepler <103641474+hljie@users.noreply.github.com>
2023-01-21 10:08:38 +0800

    Update deploy.ts
    finish

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,21 @@
-import '@nomiclabs/hardhat-ethers';
+import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  const tokenA = await FT.deploy("TokenA", "A");
+  await tokenA.deployed();
+  console.log(`token_A deployed: ${tokenA.address}`);
+
+  const tokenB = await FT.deploy("TokenB", "B");
+  await tokenB.deployed();
+  console.log(`token_B deployed: ${tokenB.address}`);
+
+  const Uniswap = await ethers.getContractFactory("Uniswap");
+  const swap = await Uniswap.deploy(tokenA.address, tokenB.address);
+  await swap.deployed();
+  console.log(`Uniswap deployed: ${swap.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
