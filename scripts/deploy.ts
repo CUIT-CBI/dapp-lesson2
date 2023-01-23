c8a1810c02e26c9ab349733a379a2ba4ac92bee2
gurongwen <2945918713@qq.com>
2023-01-23 13:54:43 +0800

    2020131113

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,13 @@ async function main() {
   const ft = await FT.deploy("CBI", "CUIT");
 
   await ft.deployed();
+  const token1 = await FT.deploy("token1", "CBI");
+  await token1.deployed();
+  const token2 = await FT.deploy("token2", "CBI");
+  await token2.deployed();
   console.log(`FT deployed to ${ft.address}`);
+   console.log(`token1 deployed to ${token1.address}`);
+  console.log(`token2 deployed to ${token2.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
