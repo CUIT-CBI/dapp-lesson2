1bd1863e7b468f7ce491c180f25c9c9260511068
STAR-FALL-00 <77623276+STAR-FALL-00@users.noreply.github.com>
2023-01-23 23:21:01 +0800

    Add files via upload

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,15 +2,23 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+    // 获取第一个账户，用来部署合约
+    const accounts = await ethers.getSigners();
+    const deployer = accounts[0];
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+    // 部署第一种代币
+    const FT1 = await ethers.getContractFactory("FT");
+    const ft1 = await FT1.deploy("token1", "t1", 18, {from: deployer});
+    await ft1.deployed();
+    console.log(`token1 contract deployed at address: ${ft1.address}`);
+
+    // 部署第二种代币
+    const FT2 = await ethers.getContractFactory("FT");
+    const ft2 = await FT2.deploy("token2", "t2", 18, {from: deployer});
+    await ft2.deployed();
+    console.log(`token2 contract deployed at address: ${ft2.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
