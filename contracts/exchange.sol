77b42d89137c28bf28ad1649b1e232b23e0152f9
flyyykings <117018803+flyyykings@users.noreply.github.com>
2023-01-23 22:37:31 +0800

    Add files via upload

---
+++ contracts/exchange.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+import "./Math.sol";
+import "./FT.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+contract ExchangeTwoTokens{
+    FT public ft;
+    IERC20 public tokenD;
+    IERC20 public tokenN;
+    constructor(address _tokenD,address _tokenN,FT _ft){
+        tokenD = IERC20(_tokenD);
+        tokenN = IERC20(_tokenN);
+        ft=_ft;
+    }
+    uint public totalD = 0;
+    uint public totalN = 0;
+    //LPtoken记录发送LPtoken总量
+    uint public LPtoken = 0;
+    uint public liquidity = 0;
+    uint public k = 0;
+    //添加流动性
+    function addLiquidity(uint addnumberD,uint addnumberN) external{
+        if (LPtoken==0) {
+            tokenD.transferFrom(msg.sender, address(this), addnumberD);
+            tokenN.transferFrom(msg.sender, address(this), addnumberN);
+            totalD += addamountD;
+            totalN += addamountN;
+            LPtoken += Math.sqrt(totalD*totalN);
+            liquidity = LPtoken;
+            k = totalD*totalN;
+            ft.mint(msg.sender, LPtoken);
+        } else {
+            uint256 transferTokenNAmount = (addnumberD+totalD)*totalN/totalD;
+            require(transferTokenNAmount==addnumberN , "这不是第一次充值，请按比例充值");
+            tokenD.transferFrom(msg.sender, address(this), addnumberD);
+            tokenN.transferFrom(msg.sender, address(this), addnumberN);
+            totalD += addnumberD;
+            totalN += addnumberN;
+            k = totalD*totalN;
+            //D和N币的变化导致流动性变化
+            liquidity = Math.sqrt(totalD*totalN);
+            LPtoken += Math.sqrt(addnumberD*addnumberN);
+            ft.mint(msg.sender, Math.sqrt(addnumberD*addnumberN));
+        }
+    }
+
+    function removeLiquidity(uint removeTokenDnumber,uint _removeTokenNnumber) external {
+        uint removeTokenNnumber =totalN - k/(totalD-removeTokenDnumber);
+        require(removeTokenNnumber== _removeTokenNnumber, " 请按比例充值");
+        //销毁LPtoken
+        ft.burn(Math.sqrt(removeTokenDnumber*removeTokenNnumber));
+        LPtoken -= Math.sqrt(removeTokenDnumber*removeTokenNnumber);
+        totalD-=removeTokenDnumber;
+        totalN-=removeTokenNnumber;
+        k = totalD*totalN;
+        //返还
+        tokenD.transfer(msg.sender, removeTokenDnumber);
+        tokenN.transfer(msg.sender, removeTokenNnumber);
+    }
+
+    function gettokenNOutnumber(uint amountD) public view returns(uint) {
+          uint tokenNOutnumber = totalN-(totalD+amountD)/k;
+          return tokenNOutnumber;
+    }
+    function gettokenDOutnumber(uint amountN) public view returns(uint) {
+          uint tokenDOutnumber = totalD-(totalN+amountN)/k;
+          return tokenDOutnumber;
+    }
+    function substitute(uint amountInputD) external {
+        uint tokenNOutnumber = gettokenNOutnumber(amountInputD);
+        tokenNOutnumber = tokenNOutnumber *997/1000;
+        //滑点
+        if(tokenNOutnumber>=tokenNOutnumber*7/10){
+            revert("回报太少");
+        }
+        tokenD.transferFrom(msg.sender, address(this),amountInputD);
+        tokenN.transfer(msg.sender,tokenNOutnumber);
+        totalD+=amountInputD;
+        totalN-=tokenNOutnumber;
+    }
+    function substitute(uint amountInputN) external {
+        uint tokenDOutnumber = gettokenDOutnumber(amountInputN);
+        tokenDOutnumber = tokenDOutnumber *997/1000;
+        if(tokenDOutnumber>=tokenDOutnumber*7/10){
+            revert("回报太少");
+        }
+        tokenN.transferFrom(msg.sender, address(this),amountInputN);
+        tokenD.transfer(msg.sender,tokenDOutnumber);
+        totalN+=amountInputN;
+        totalD-=tokenDOutnumber;
+    }
+
+
+}
