98a22d69e4ec28b065470a86f75f3e30418ba2e7
zhangpan <1846766231@qq.com>
2023-01-23 23:49:56 +0800

    2020131038

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,19 @@ async function main() {
   const ft = await FT.deploy("CBI", "CUIT");
 
   await ft.deployed();
+
+  const token0 = await FT.deploy("token0", "ZP0");
+  await token0.deployed();
+  const token1 = await FT.deploy("token1", "ZP1");
+  await token1.deployed();
+  const exchange = await FT.deploy(token0.address,token1.address);
+  await exchange.deployed();
+
   console.log(`FT deployed to ${ft.address}`);
+
+  console.log(`token0 address: ${token0.address}`);
+  console.log(`token1 address: ${token1.address}`);
+  console.log(`exchange address: ${exchange.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
