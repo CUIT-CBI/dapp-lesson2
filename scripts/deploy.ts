40a4edf2b249ef9586c9c426e1540e9a3c121479
46ld1123 <2386178894@qq.com>
2023-01-24 11:45:59 +0800

    uniswap

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,10 +3,22 @@ import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const tokenA = await FT.deploy("CBI", "CUIT");
+  const tokenB = await FT.deploy("LD", "LZQ");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  await tokenA.deployed();
+  await tokenB.deployed();
+  console.log(`tokenA deployed to ${tokenA.address}`);
+  console.log(`tokenB deployed to ${tokenB.address}`);
+  const Factory = await ethers.getContractFactory("Factory");
+  const factory = await Factory.deploy();
+  await factory.deployed();
+  console.log(`factory deployed to ${factory.address}`);
+  const Router = await ethers.getContractFactory("Router");
+  const router = await Router.deploy(factory.address);
+  await router.deployed();
+  console.log(`router deployed to ${router.address}`);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
