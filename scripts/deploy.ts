060de4d924502db928600665a6289f3166f2007b
wly703 <2913558953@qq.com>
2023-01-24 23:27:55 +0800

    1

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,17 @@
 import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
-
 async function main() {
   const FT = await ethers.getContractFactory("FT");
   const ft = await FT.deploy("CBI", "CUIT");
 
   await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
-}
+  const tokena = await FT.deploy("tokena", "W1");
+  await tokena.deployed();
+  const tokenb = await FT.deploy("tokenb", "W2");
+  await tokenb.deployed();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.log(`FT deployed to ${ft.address}`);
+  console.log(`tokena deployed to ${tokena.address}`);
+  console.log(`tokenb deployed to ${tokenb.address}`);
+
+}
