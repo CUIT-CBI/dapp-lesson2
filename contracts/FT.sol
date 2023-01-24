c720497d4ef68a1828fceb609006799c639e08bc
tencyTT <116853741+tencyTT@users.noreply.github.com>
2023-01-24 23:48:33 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -1,14 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20, Pausable, Ownable {
+contract FT is ERC20, Pausable, Ownable{
+    
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
     
-    function mint(address account, uint256 amount) external onlyOwner {
+    //映射存在交易对的地址
+    mapping(address => bool) pairs;
+
+    modifier OnlyPair(){
+        require(pairs[msg.sender] == true);
+        _;
+    }
+    //设置交易对提供者
+    function setPair(address pair) public {
+        pairs[pair] = true;
+    }
+    
+    function mint(address account, uint256 amount) external OnlyPair {
         _mint(account, amount);
     }
 
@@ -16,15 +28,4 @@ contract FT is ERC20, Pausable, Ownable {
         _burn(msg.sender, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-    
-    function pause() external onlyOwner {
-        _pause();
-    }
-    
-    function unpause() external onlyOwner {
-        _unpause();
-    }
 }
