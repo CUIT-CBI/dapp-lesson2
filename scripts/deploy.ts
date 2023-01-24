93fb625628d6de1a128efd3b3eb8ae1dbcdc930f
2020131118 <116857998+1793129577a@users.noreply.github.com>
2023-01-24 16:10:05 +0800

    Add files via upload

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,32 @@
 import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
-async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  async function main() {
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    const FT = await ethers.getContractFactory("FT");
+    const ft = await FT.deploy("CBI", "CUIT");
+    const Factory = await ethers.getContractFactory("TokenUse");
+    const tokenA = await FT.deploy("A", "A");
+    const tokenB = await FT.deploy("B", "B");
+    await tokenA.deployed();
+    await tokenB.deployed();
+    console.log(`FT deployed to ${tokenA.address}`);
+    console.log("FT address:",tokenB.address);
+  
+    const factory = await Factory.deploy();
+    await factory.deployed();
+    console.log(`factory address:${factory.address}`);
+  
+    await factory.createPair(tokenA.address,tokenB.address);
+    const pairAddress = await factory.getPair(tokenA.address,tokenB.address);
+    const pairFactory = await ethers.getContractFactory("tokenPair");
+    const pair =  pairFactory.attach(pairAddress);
+  
+    await pair.deployed()
+    console.log(`pair address: ${pair.address}`)
+    console.log(`tokenA: ${await pair.A()} tokenB: ${await pair.B()}`)
+  
+  
+    await ft.deployed();
+    console.log(`FT deployed to ${ft.address}`);
+  };
