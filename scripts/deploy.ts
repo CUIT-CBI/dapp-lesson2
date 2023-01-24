f68f60187b86e46ca4b26c4d16847e8f9589e8ee
FLY <74313026+GOODFLYO@users.noreply.github.com>
2023-01-24 22:12:30 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,28 @@
 import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
-
 async function main() {
+  //部署工厂合约
+  const  MyFactory = await ethers.getContractFactory("FTFactory");
+  const factory = await MyFactory.deploy();
+  await factory.deployed();
+  console.log(`factory address:${factory.address}`);
+  
+  //部署A B token
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const tokenA = await FT.deploy("tokenA", "ZYN_A");
+  await tokenA.deployed();
+  
+  const tokenB = await FT.deploy("tokenB", "ZYN_B");
+  await tokenB.deployed();
+  console.log(`tokenA deployed to ${tokenA.address}`);
+  console.log(`tokenB deployed to ${tokenB.address}`);
+  
+  //创建token对
+  const pair = await factory.createPairs(tokenA.address,tokenB.address);
+  console.log(`pair deployed to ${pair.address}`);
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
