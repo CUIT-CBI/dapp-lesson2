d5f87ed1f0438a46c8da6360a15e2abaac4c06eb
QSRYH <119659352+QSRYH@users.noreply.github.com>
2023-01-25 00:47:34 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,30 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+    // 创建部署 coinA 和 coinB
+  const FT = await ethers.getContractFactory("FT");
+  const uniswapFactory = await ethers.getContractFactory("factory");
+  const coin_A = await FT.deploy("coin_A", "coin_A");
+  await coin_A.deployed();
+  console.log(`coin_A deployed to ${coin_A.address}`);
+  
+  const coin_B = await FT.deploy("coin_B", "coin_B");
+  await coin_B.deployed();
+  console.log("coin_B address:",coin_B.address);
+
+  const Factory = await uniswapFactory.deploy();
+  await Factory.deployed();
+  console.log(`uniswapFactory address:${uniswapFactory.address}`);
+
+  // 使用 factory创建uniswap合约
+  await Factory.createPair(coin_A.address,coin_B.address,"平台方的地址");
+  const uniswapAddress = await Factory.getPair(coin_A.address,coin_B.address);
+  const Contract = await ethers.getContractFactory("swap");
+  const uniswap =  Contract.attach(uniswapAddress);
+
+  await uniswap.deployed()
+  console.log(`uniswap address: ${uniswap.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
