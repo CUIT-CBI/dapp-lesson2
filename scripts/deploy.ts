aacb21630f327e63f868b0112b82b737383d956a
wjy0324 <106463185+wjy0324@users.noreply.github.com>
2023-01-24 17:09:32 +0800

    Add files via upload

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,10 +3,15 @@ import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const tokenA = await FT.deploy("CBI", "CUIT");
+  await tokenA.deployed();
+  const tokenB = await FT.deploy("WJY", "WJY");
+  await tokenB.deployed();
+  const funcRealize = await FT.deploy(tokenA.address, tokenB.address);
+  await funcRealize.deployed();
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  console.log(`token1 deployed to ${tokenA.address}`);
+  console.log(`token2 deployed to ${tokenB.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
