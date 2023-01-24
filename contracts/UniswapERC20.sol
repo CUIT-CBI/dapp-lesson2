118ec64635b1d583fd35c326a584411fd561f2a7
AloneAtWar <2318583515@qq.com>
2023-01-24 14:19:17 +0800

    第一个简易版本

---
+++ contracts/UniswapERC20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract UniswapERC20 is ERC20{
+    constructor() ERC20("Csd Uniswap","Csd"){
+
+    }
+
+
+}
