13dfc488f4e5b3a9467fbc61dbb8149a9acfb8a9
xfzpml <118715222+xfzpml@users.noreply.github.com>
2023-01-24 11:20:06 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,12 +2,16 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+    const tokenA = "0x0000000000000000";    
+    const tokenB = "0x1111111111111111";    
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+    const PmlPair = await ethers.getContractFactory('PmlPair');
+    const pmlpair = await PmlPair.deploy(tokenA, tokenB);
+
+    await pmlpair.deployed();
+    console.log(`Contract deployed to ${pmlpair.address}`);
 }
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
