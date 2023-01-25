7f337eb1bb93f1d85c70ad27505651f2e2a4b134
dhxqq <119414771+dhxqq@users.noreply.github.com>
2023-01-25 21:47:47 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,28 @@
 import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
-async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
-}
+async function main() {
+    const FT = await ethers.getContractFactory("FT");
+
+    const tokenA = await FT.deploy("TokenA", "ZYQ");
+    await tokenA.deployed();
+    console.log(`tokenA deployed to ${tokenA.address}`);
+
+    const tokenB = await FT.deploy("TokenB", "zyq");
+    await tokenB.deployed();
+    console.log(`tokenB deployed to ${tokenB.address}`);
+
+    const TokenSwap = await FT.deploy(tokenA.address, tokenB.address);
+    await TokenSwap.deployed();
+    console.log(`Pair deployed to ${TokenSwap.address}`);
+
+
+
+
+
+  }
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
