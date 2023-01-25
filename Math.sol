d370233ed406bd126e1c83d301f598f0392b95a2
Casper-mao <118877937+Casper-mao@users.noreply.github.com>
2023-01-25 21:26:49 +0800

    Create Math.sol

---
+++ Math.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+library Math {
+    function lit(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = x < y ? x : y;
+    }
+
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+}
