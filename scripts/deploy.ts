72c0d67ce9cb5eae6d2270cd26976e2d52d86017
cyyyy111 <2645726908@qq.com>
2023-01-26 00:09:22 +0800

    作业

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,13 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const token1="111";
+  const token2="222";
+  const tokenpair = await ethers.getContractFactory("TokenPair");
+  const TokenPair = await tokenpair.deploy(token1, token2);
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  await TokenPair.deployed();
+  console.log(`FT deployed to ${TokenPair.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
