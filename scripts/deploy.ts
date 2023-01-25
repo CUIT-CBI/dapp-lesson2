e631b94a504169ee8cdeb9a766a43443bb735d38
Gry1223 <119167905+Gry1223@users.noreply.github.com>
2023-01-25 14:47:09 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,9 +2,12 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const myToken =await MyToken.deploy();
+  await myToken.deployed();
+  console.log("Token Address:", myToken.address);
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
-
+  const ft = await FT.deploy("CBI", "CUIT",myToken.address);
   await ft.deployed();
   console.log(`FT deployed to ${ft.address}`);
 }
