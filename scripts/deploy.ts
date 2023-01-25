3d5600aeef2cc834a6fa95a7548b7e0d154c129f
lixinyuan <1902699166@qq.com>
2023-01-25 21:50:31 +0800

    2020131115

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,16 @@
 import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
-  async function main() {
+async function main() {
+  const FT = await ethers.getContractFactory("FT");
+  const ft = await FT.deploy("CBI", "CUIT");
 
-    const FT = await ethers.getContractFactory("FT");
-    const SwapPool = await ethers.getContractFactory("SwapPool");
-    const token0 = await FT.deploy("token0", "t0");
-    const token1 = await FT.deploy("token1", "t1");
-    await token0.deployed();
-    await token1.deployed();
-    
+  await ft.deployed();
 
-    const factory = await  SwapPool.deploy();
-    await factory.deployed();
-    console.log(`factory address:${factory.address}`);
-
-    await factory.createPair(token0.address,token1.address);
-    const pairAddress = await factory.getPair(token0.address,token1.address);
-    const pairFactory = await ethers.getContractFactory("tokenPair");
-    const pair =  pairFactory.attach(pairAddress);
-    await pair.deployed()
-
-  };
+  const token0 = await FT.deploy("token0", "t0");
+  await token0.deployed();
+  const token1 = await FT.deploy("token1", "t1");
+  await token1.deployed();
+  const contract = await FT.deploy(token0.address, token1.address);
+  await contract.deployed();
+}
