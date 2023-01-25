73af8b3b78ee96e931e2289906a5fa56eb2a6b6a
LEO12-G <85423716+LEO12-G@users.noreply.github.com>
2023-01-25 10:26:03 +0800

    Add files via upload

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,15 +3,23 @@ import { ethers } from "hardhat";
 
 async function main() {
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const ft = await FT.deploy("GSY", "CBI");
 
   await ft.deployed();
   console.log(`FT deployed to ${ft.address}`);
+
+  const UniswapV2Pair = await ethers.getContractFactory("UniswapV2Pair");
+  const uniswapV2Pair = await UniswapV2Pair.deploy();
+
+  await uniswapV2Pair.deployed();
+  console.log(`FT deployed to ${uniswapV2Pair.address}`);
+
+  const Exchange = await ethers.getContractFactory("Exchange");
+  const exchange = await Exchange.deploy();
+
+  await exchange.deployed();
+  console.log(`FT deployed to ${exchange.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
