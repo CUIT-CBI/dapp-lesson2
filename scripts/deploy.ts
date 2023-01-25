1f86929322727cfd05b46930ef667b153972d110
supredu <1020665094@qq.com>
2023-01-25 15:56:01 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,32 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
+//部署ERC20币A合约
   const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const tokenA = await FT.deploy("TokenA", "A");
+  await tokenA.deployed();
+  const addressA = tokenA.address;
+//部署ERC20币B合约
+  const tokenB = await FT.deploy("TokenB", "B");
+  await tokenB.deployed();
+  const addressB = tokenB.address;
+//部署DHLswap合约
+  const DHLswap = await ethers.getContractFactory("DHLswap");
+  const swap = await DHLswap.deploy(addressA, addressB);
+  await swap.deployed();
+  const swapAddress = swap.address;
+//部署DHLrewardtoken合约
+  const DHLrewardtoken = await ethers.getContractFactory("DHLrewardtoken");
+  const rewardToken = await DHLrewardtoken.deploy();
+  await rewardToken.deployed();
+  const rewardAddress = rewardToken.address;
+//部署DHLstaking合约
+  const DHLstaking = await ethers.getContractFactory("DHLstaking");
+  const staking = await DHLstaking.deploy(swapAddress, rewardAddress);
+  await staking.deployed();
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  console.log("contracts deployment finished");
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
