7d0fd3035b09282096dd6a3440d71284266efa46
umi1028 <114017189+umi1028@users.noreply.github.com>
2023-01-25 08:19:10 +0800

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,9 +4,19 @@ import { ethers } from "hardhat";
 async function main() {
   const FT = await ethers.getContractFactory("FT");
   const ft = await FT.deploy("CBI", "CUIT");
-
+  const token0 = await FT.deploy("XYM","XYM"); 
+  const token1 = await FT.deploy("xu","xu"); 
+  const Pair = await ethers.getContractFactory("pair");
+  const pair = await Pair.deploy(token0.address,token1.address);
   await ft.deployed();
+  await pair.deployed();
   console.log(`FT deployed to ${ft.address}`);
+  console.log(`Pair deployed to ${pair.address}`);
+  await token0.setPair(token0.address);
+  await token0.setPair(token1.address);
+  await token0.mint(token0.address,1000000000);
+  await token1.mint(token1.address,1000000000);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
