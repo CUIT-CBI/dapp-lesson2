f71eb0a3732e1bf05099614f723ac1e01c122574
TickledPinkZyh <109411375+TickledPinkZyh@users.noreply.github.com>
2023-02-19 13:36:36 +0800

    Update deploy.ts
    2020131133更正

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,29 @@
-import '@nomiclabs/hardhat-ethers';
-import { ethers } from "hardhat";
-let deployer
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
-  const token1 = await FT.deploy("CBI", "CUIT");
-  const token2 = await FT.deploy("ZYH", "ZYH");
-  const Factory = await ethers.getContractFactory("Factory");
-  const factory = await Factory.deploy(ft.address);
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
-  await token1.setPair(deployer.address)
-  await token2.setPair(deployer.address)
-  await token1.mint(deployer.address,10000000)
-  await token2.mint(deployer.address,10000000)
-  await token1.approve(factory.address,10000000)
-  await token2.approve(factory.address,10000000)
-  await factory.deployed();
-  console.log(`Factory deployed to ${factory.address}`);
-}
+    const FT = await ethers.getContractFactory("FT");
+    const ft = await FT.deploy("CBI", "CUIT");
+    
+    const token0 = await FT.deploy("token1", "Zyh");
+    await token0.deployed();
+    const token1 = await FT.deploy("token2", "ZSY");
+    await token1.deployed();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.log(`token0 deployed to ${token0.address}`);
+    console.log(`token1 deployed to ${token1.address}`);
+  
+    await ft.deployed();
+
+    console.log(`FT deployed to ${ft.address}`);
+
+    const Pair = await ethers.getContractFactory("pair");
+    const pair = await Pair.deploy(token0.address, token1.address);
+
+    await pair.deployed();
+    console.log(`Pair deployed to ${pair.address}`);
+  }
+
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  }
+  );
