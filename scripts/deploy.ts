3b032473bff7b2c2ff326b37e137fe3ac77d1e51
AloneAtWar <2318583515@qq.com>
2023-02-22 14:50:33 +0800

    吴思城的最后一次实验

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,14 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from "hardhat";
 
 async function main() {
-  const FT = await ethers.getContractFactory("FT");
-  const ft = await FT.deploy("CBI", "CUIT");
+  const token1="11111";
+  const token2="22222";
 
-  await ft.deployed();
-  console.log(`FT deployed to ${ft.address}`);
+  const PAIR = await ethers.getContractFactory("WSCPair");
+  const pair = await PAIR.deploy(token1, token2);
+
+  await pair.deployed();
+  console.log(`pair deployed to ${pair.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
