d38f2d407cc3ae8fe59167a12a90ed2e8eb4b129
DorisChe <93329107+Cheyukj@users.noreply.github.com>
2023-02-23 09:00:01 +0800

    Create FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -7,29 +7,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FT is ERC20, Pausable, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-    
+
     function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    function burn(uint256 amount) external {
+    function burn(address account, uint256 amount) external {
         _burn(msg.sender, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-    
     function pause() external onlyOwner {
         _pause();
     }
-    
+
     function unpause() external onlyOwner {
         _unpause();
-    }    
-    
-    function setPair(address pair)public onlyOwner{
-        pairs[pair]=true;
     }
-
+    
 }
