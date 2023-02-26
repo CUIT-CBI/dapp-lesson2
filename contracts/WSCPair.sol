450518a93c97d29291389cf993cd4246e7d50e5f
黄鑫羿 <1625420287@qq.com>
2023-02-26 17:58:38 +0800

    update

---
+++ contracts/WSCPair.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract WSCPair is ERC20 {
+
+
+    IERC20 public token0;
+    IERC20 public token1;
+
+
+    constructor(address _token0, address _token1) ERC20("WSC pair","WSC"){
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+
+    }
+
+    function addLiquidity(uint amount0, uint amount1) public returns (uint liquidity) {
+        (uint256 balance0,uint256 balance1)=getStatus();
+        token0.transferFrom(msg.sender, address(this), amount0);
+        token1.transferFrom(msg.sender, address(this), amount1);
+
+        uint _totalSupply = totalSupply();
+        liquidity = Math.min(amount0 * _totalSupply / balance0, amount1 * _totalSupply / balance1);
+        require(liquidity > 0, "liquidity less than zero");
+        _mint(msg.sender, liquidity);
+
+    }
+
+    function removeLiquidity(uint liquidity)public  {
+        uint _totalSupply = totalSupply();
+
+        (uint256 balance0,uint256 balance1)=getStatus();
+        uint amount0 = liquidity * _totalSupply / balance0;
+        uint amount1 = liquidity * _totalSupply / balance1;
+        _burn(msg.sender, liquidity);
+
+        token0.transfer(msg.sender, amount0);
+        token1.transfer(msg.sender, amount1);
+
+
+    }
+
+
+    function swap(IERC20 token,uint amount,uint maxSlippage) public returns(uint slippage){
+        IERC20 from=token0;
+        IERC20 to=token1;
+        (uint256 fromBalance,uint256 toBalance)=getStatus();
+        if (token==token1){
+            IERC20 temp=from;
+            from=to;
+            to=temp;
+        }else if (token!=token0){
+            require(token==token0,"err token");
+        }
+        uint256 swapAmount = (toBalance - fromBalance * toBalance/ (fromBalance + toBalance)) * (1000-3) / 1000;
+        uint256 targetAmount = toBalance * swapAmount / fromBalance;
+        slippage = (targetAmount - swapAmount) * 100 / targetAmount;
+        if(maxSlippage!=0){
+            require(maxSlippage<=slippage,"slippage too large");
+        }
+        from.transferFrom(msg.sender, address(this), amount);
+        token1.transfer(msg.sender, swapAmount);
+
+
+    }
+
+
+
+    function getStatus() public view returns(uint256 balance0,uint256 balance1){
+        balance0 = token0.balanceOf(address(this));
+        balance1 = token1.balanceOf(address(this));
+    }
+
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,65 +1,18 @@
 import { ethers } from "hardhat";
-let deployer
+
 async function main() {
-  const T1 = await ethers.getContractFactory("FT");
-  const t1 = await T1.deploy("sam","SAM");
-
-  const T2 = await ethers.getContractFactory("FT");
-  const t2 = await T2.deploy("syz","SYZ");
-  
-
-  const zzzz = await ethers.getContractFactory("Exchange");
-  const z1z = await zzzz.deploy('zzzzzz', 'zwzwzw');
-
-  await z1z.deployed();
-
-  console.log(`t1address: ${t1.address} t2address: ${t2.address}`);
   const FT = await ethers.getContractFactory("FT");
-  const tokenA = await FT.deploy("YZW", "Y");
-  await tokenA.deployed();
-  const tokenB = await FT.deploy("YZW", "Z");
-  await tokenB.deployed();
+  const ft = await FT.deploy("CBI", "CUIT");
+  const token1="11111";
+  const token2="22222";
 
-  const swap = await ethers.getContractFactory("swap");
-  const swaped = await swap.deploy(tokenA.address, tokenB.address);
-  await swaped.deployed();
+  await ft.deployed();
+  console.log(`FT deployed to ${ft.address}`);
+  const PAIR = await ethers.getContractFactory("WSCPair");
+  const pair = await PAIR.deploy(token1, token2);
 
-  console.log(`FT deployed to ${tokenA.address}`);
-  console.log(`FT deployed to ${tokenB.address}`);
-
-  [deployer] = await ethers.getSigners();
-  const provider = await ethers.providers.getDefaultProvider();
-  const WETHContract = await ethers.getContractFactory("WETH");
-  const WETH = await WETHContract.deploy();
-  await WETH._deployed();
-
-  const factoryContract = await ethers.getContractFactory("Factory");
-  const factory = await factoryContract.deploy(); 
-  await factory.deployed();
-
-  const routerContract = await ethers.getContractFactory("Router");
-  const router = await routerContract.deploy(factory.address, WETH.address);
-  await router.deployed();
-
-  const stakerpoolContract = await ethers.getContractFactory("GzxERC20");
-  const stakerpool = await stakerpoolContract.deploy();
-  await WETH._deployed();
-  let blocknumber_pre = provider.getBlockNumber();
-
-  const stakerContract = await ethers.getContractFactory("StakerMain");
-  const staker = await stakerContract.deploy(stakerpool.address, deployer.address, blocknumber_pre, 999999999, provider.getBlockNumber());
-  await WETH._deployed();
-
-
-  console.log("WETH's address : ", WETH.address, "\n"+
-              "Factory's address : ", factory.address, "\n"+
-              "Router's address : ", router.address, "\n" +
-              "staker's address : ", staker.address);
+  await pair.deployed();
+  console.log(`pair deployed to ${pair.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
